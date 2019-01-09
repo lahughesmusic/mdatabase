73f5748f421e4c766528f5928eb4a6ab1d860672
Lea Hughes <lahughesmusic@gmail.com>
2019-01-09 13:22:00 -0600

    add files

---
+++ src/App.js
@@ -0,0 +1,164 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <a href="../resources/100_Years.pdf" download>
+          <h1>100 Years</h1>
+        </a>
+        <a href="../resources/Barcarolle.pdf" download>
+          <h1>Barcarolle</h1>
+        </a>
+        <a href="../resources/Blue_Danube_Waltz.pdf" download>
+          <h1>Blue Danube Waltz</h1>
+        </a>
+        <a href="../resources/Clocks.pdf" download>
+          <h1>Clocks</h1>
+        </a>
+        <a href="../resources/Cruella_De_Vil.pdf" download>
+          <h1>Cruella De Vil</h1>
+        </a>
+        <a href="../resources/Deliver_Us.pdf" download>
+          <h1>Deliver Us</h1>
+        </a>
+        <a href="../resources/Dance_of_the_SugarPlum_Fairy.pdf" download>
+          <h1>Dance of the Sugar Plum Fairy</h1>
+        </a>
+        <a href="../resources/Eency_Weency_Spider.pdf" download>
+          <h1>Eency Weency Spider</h1>
+        </a>
+        <a href="../resources/Fallin.pdf" download>
+          <h1>Fallin'</h1>
+        </a>
+        <a href="../resources/Fur_Elise.pdf" download>
+          <h1>Fur Elise</h1>
+        </a>
+        <a href="../resources/Gymnopdedie_I.pdf" download>
+          <h1>Gymnopdie</h1>
+        </a>
+        <a href="../resources/Hall_of_the_Mountain_King.pdf" download>
+          <h1>Hall of the Mountain King</h1>
+        </a>
+        <a href="../resources/Hey_Diddle_Diddle.pdf" download>
+          <h1>Hey Diddle Diddle</h1>
+        </a>
+        <a href="../resources/Hickory_Dickory_Dock.pdf" download>
+          <h1>Hickory Dickory Dock</h1>
+        </a>
+        <a href="../resources/I_Will_Always_Love_You.pdf" download>
+          <h1>I Will Always Love You</h1>
+        </a>
+        <a>
+          <h1>Jesu Joy of Man Desiring</h1>
+        </a>
+        <a>
+          <h1>Jurassic Park</h1>
+        </a>
+        <a>
+          <h1>Lazy Mary</h1>
+        </a>
+        <a>
+          <h1>Lean On Me</h1>
+        </a>
+        <a>
+          <h1>Let it Go</h1>
+        </a>
+        <a>
+          <h1>Little Boy Blue</h1>
+        </a>
+        <a>
+          <h1>Little Dog Gone</h1>
+        </a>
+        <a>
+          <h1>Menuet in G Minor</h1>
+        </a>
+        <a>
+          <h1>Musettas Song</h1>
+        </a>
+        <a>
+          <h1>My Favorite Things</h1>
+        </a>
+        <a>
+          <h1>Old Macdonald</h1>
+        </a>
+        <a>
+          <h1>Old Mother Hubbard</h1>
+        </a>
+        <a>
+          <h1>Old Woman In The Shoe</h1>
+        </a>
+        <a>
+          <h1>Over The Rainbow</h1>
+        </a>
+        <a>
+          <h1>Canon in D</h1>
+        </a>
+        <a>
+          <h1>Piano Man</h1>
+        </a>
+        <a>
+          <h1>Pink Panther</h1>
+        </a>
+        <a>
+          <h1>Pop Goes The Weasel</h1>
+        </a>
+        <a>
+          <h1>Prelude in A Major</h1>
+        </a>
+        <a>
+          <h1>Prelude in D Minor</h1>
+        </a>
+        <a>
+          <h1>Prelude in E Minor</h1>
+        </a>
+        <a>
+          <h1>Russian Folk Song</h1>
+        </a>
+        <a>
+          <h1>See You Again</h1>
+        </a>
+        <a>
+          <h1>Someday</h1>
+        </a>
+        <a>
+          <h1>Somewhere Out There</h1>
+        </a>
+        <a>
+          <h1>Sonata in C</h1>
+        </a>
+        <a>
+          <h1>Sonata in G</h1>
+        </a>
+        <a>
+          <h1>Stay</h1>
+        </a>
+        <a>
+          <h1>Tears in Heaven</h1>
+        </a>
+        <a>
+          <h1>Tennessee</h1>
+        </a>
+        <a>
+          <h1>Theme From E.T.</h1>
+        </a>
+        <a>
+          <h1>This Old Man</h1>
+        </a>
+        <a>
+          <h1>Tiny Dancer</h1>
+        </a>
+        <a>
+          <h1>Toreador's Song</h1>
+        </a>
+        <a>
+          <h1>When You Believe</h1>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default App;
