0209680c0ac0b1bc034acd31921be82dffb260e6
Lea Hughes <lahughesmusic@gmail.com>
2019-01-10 17:45:42 -0600

    add files

---
+++ build/precache-manifest.f037dbfb998c50f70127698a8be1a802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lahughesmusic/mdatabase/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ef5b84ebbaec8f7c33ce",
+    "url": "/lahughesmusic/mdatabase/static/js/runtime~main.ef5b84eb.js"
+  },
+  {
+    "revision": "4f21270e903d2cdbda78",
+    "url": "/lahughesmusic/mdatabase/static/js/main.4f21270e.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/lahughesmusic/mdatabase/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "4f21270e903d2cdbda78",
+    "url": "/lahughesmusic/mdatabase/static/css/main.fb84b8c3.chunk.css"
+  },
+  {
+    "revision": "94625f518f8e6a08ca1466e138ae06a1",
+    "url": "/lahughesmusic/mdatabase/index.html"
+  }
+];
